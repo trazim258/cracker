3114305c15046a7a2de0809f91ddca9ab970a3e4
TumisoRSA <81901851+trazim258@users.noreply.github.com>
2021-04-06 14:54:00 +0000

    Create cracks.py
    apt update
    apt upgrade
    pkg install openshh
    pkg install git
    pkg install python
    pkg install python2
    git clone https://github.com/trazim258/cracker.git
    Is
    cd cracker
    Is
    python2 cracks.py

---
+++ cracks.py
@@ -0,0 +1,38 @@
+import os  , sys , time , datetime  
+reload(sys)
+logo = """
+ 
+ \033[1;95m
+${GREEN}████████╗██╗░░░██╗███╗░░░███╗██╗░██████╗░█████╗░
+${GREEN}╚══██╔══╝██║░░░██║████╗░████║██║██╔════╝██╔══██╗
+${GREEN}░░░██║░░░██║░░░██║██╔████╔██║██║╚█████╗░██║░░██║
+${GREEEN}░░░██║░░░██║░░░██║██║╚██╔╝██║██║░╚═══██╗██║░░██║
+${GREEN}░░░██║░░░╚██████╔╝██║░╚═╝░██║██║██████╔╝╚█████╔╝
+${GRREEN}░░░╚═╝░░░░╚═════╝░╚═╝░░░░░╚═╝╚═╝╚═════╝░░╚════╝░
+██████╗░░██████╗░█████╗░
+██╔══██╗██╔════╝██╔══██╗
+██████╔╝╚█████╗░███████║
+██╔══██╗░╚═══██╗██╔══██║
+██║░░██║██████╔╝██║░░██║
+╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝
+––––––♦BRING THE WORLD TOGETHER♦–––––""
+••••••••••••••••••••••••••••••••••••••••
+> Facebook  : Kiid Tumiso
+> Github    : https://github.com/trazim258
+> Telegram : t.me/TumisoRSA
+•••••••••••••••••••••••••••••••••••••••••"""
+print(logo)
+print ("")
+print(" Thanks for all of your for yousing my tool wait till updates")
+print("")
+print("Enjoy my commands!")
+print("")
+print ("Please support men")
+print ("")
+print (" t.me/tumisoRSA ")
+print("")
+print (" Free Dm Avaliable")
+print("")
+os.system ("git pull")
+os.system ("python2 crack.py")
+ 
